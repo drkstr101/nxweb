4bd8fb5891c4d3638a7bb65b685440f263416c11
Aaron R Miller <amiller@watheia.org>
2021-10-07 19:29:57 +0000

    add bit components

--- apps/www/pages/_app.tsx
+++ apps/www/pages/_app.tsx
@@ -6,18 +6,9 @@ function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to www!</title>
+        <title>Watheia Labs</title>
       </Head>
-      <div className="app">
-        <header className="flex">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src="/nx-logo-white.svg" alt="Nx logo" width="75" height="50" />
-          <h1>Welcome to www!</h1>
-        </header>
-        <main>
-          <Component {...pageProps} />
-        </main>
-      </div>
+      <Component {...pageProps} />
     </>
   )
 }
