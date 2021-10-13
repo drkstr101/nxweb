10fde1e29e750212b34bf1599b1266f80b8d8051
Aaron R Miller <amiller@watheia.org>
2021-10-13 15:47:02 -0700

    Add nextjs-with-supabase-auth stubs
    Signed-off-by: Aaron R Miller <amiller@watheia.org>

---
+++ apps/nextjs-with-supabase-auth/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: "nextjs-with-supabase-auth",
+  preset: "../../jest.preset.js",
+  transform: {
+    "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)": "@nrwl/react/plugins/jest",
+    "^.+\\.[tj]sx?$": "babel-jest"
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  coverageDirectory: "../../coverage/apps/nextjs-with-supabase-auth"
+}
